aa56395691922cf0581450d5e0bbb8837baad2b5
ThibautBaudry <thibaut.baudry@gmail.com>
2025-06-18 16:21:35 +0200

    Links we need

--- contracts/UniswapDeFi.sol
+++ contracts/UniswapDeFi.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.28;
 
-
+/*
+https://docs.uniswap.org/contracts/v3/reference/deployments
+routeur: 0xE592427A0AEce92De3Edee1F18E0157C05861564
+Link token: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+Weth9: 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
+*/
 
 
